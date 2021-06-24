7a212066b009f5784d509e892d74c89006f25c4d
javiergpx <66230279+javiergpx@users.noreply.github.com>
2021-06-24 19:31:09 -0400

    Add files via upload

--- principal.java
+++ principal.java
@@ -2,27 +2,32 @@ package proyecto;
 
 import javax.swing.*;
 
+
 import java.awt.*;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class principal {
 
+
     public static void main (String[] args){
+
 
         JFrame panel = new JFrame("Corredora Propiedades"); //JPanel
         JDesktopPane escritorio = new JDesktopPane(); //JDesktopPane
         panel.getContentPane().add(escritorio);
         panel.setExtendedState(panel.MAXIMIZED_BOTH); //Maximizado al abrir
         panel.setDefaultCloseOperation(EXIT_ON_CLOSE); //Termina la ejecucion al cerrar ventana
+        escritorio.setSize(1000,500);
+        escritorio.setVisible(true);
 
-        JMenuBar barraMenu = new JMenuBar(); 
-        panel.add(barraMenu);
+
+        barramenu barra = new barramenu();
+        barra.setBounds(0,0,1000,50);
+        barra.setVisible(true);
 
 
-        panel.add(escritorio);
-
-        panel.setSize(1000,500); //definir tamaño para la ventana principal
+        panel.setSize(1000,500); //definir tamaño para la ventana principal (1000px ancho X 500px alto)
         panel.setVisible(true);
 
 
